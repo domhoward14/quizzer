b951ad8868f736314c96aa3249fb9ecf3d434c70
ehoward <domhoward14@gmail.com>
2023-02-10 21:54:37 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:destini_challenge_starting/story_brain.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  StoryBrain sb = StoryBrain();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/background.png'),
+                fit: BoxFit.fitWidth)),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    sb.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    this.setState(() {
+                      //Choice 1 made by user.
+                      sb.nextStory(0);
+                    });
+                  },
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  child: Text(
+                    sb.getChoice1(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: sb.isFinished(),
+                  child: TextButton(
+                    onPressed: () {
+                      this.setState(() {
+                        //Choice 2 made by user.
+                        sb.nextStory(1);
+                      });
+                    },
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    child: Text(
+                      sb.getChoice2(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
